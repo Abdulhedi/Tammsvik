b939b9b7a65cac522a998f91a24740ccc1b878e9
Mårten af Sandeberg <martenaf.sandeberg@knowit.se>
2023-05-05 17:33:28 +0200

    Material UI in persona

--- tammsvik/src/components/persona.tsx
+++ tammsvik/src/components/persona.tsx
@@ -1,5 +1,11 @@
 import { Configuration, OpenAIApi } from "openai";
 import React, { useState, useEffect } from "react";
+import { Button, TextField, Box } from '@mui/material';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import LinearProgress from '@mui/material/LinearProgress';
+import { indigo } from '@mui/material/colors';
+
+const color = indigo[900];
 
 const configuration = new Configuration({
     apiKey: "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",
@@ -17,6 +23,7 @@ export default function Persona(props: IPersonaProps): JSX.Element {
     const [name, setName] = useState<string>("");
     const [description, setDescription] = useState<string>("");
     const [persona, setPersona] = useState<string>("");
+    const [creatingPersona, setCreatingPersona] = useState<boolean>(false);
 
     // useEffect(() => {
     //     if (name !== "" && persona !== ""){
@@ -28,7 +35,7 @@ export default function Persona(props: IPersonaProps): JSX.Element {
     async function createPersona(): Promise<void> {
         if (name !== "" && description !== "") {
 
-
+            setCreatingPersona(true);
             const completion = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages: [{ role: "user", content: `Create a persona description from the following, name ${name}, is ${props.isPro ? " is pro" : "is against"} ${props.topic} and personel description ${description}` }],
@@ -38,59 +45,29 @@ export default function Persona(props: IPersonaProps): JSX.Element {
                 //props.setPersona(completion.data.choices[0].message.content);
                 setPersona(completion.data.choices[0].message.content)
             }
+            setCreatingPersona(false);
         }
     }
 
     return (
         <div>
-            <input placeholder="Name" name="name" type="text" value={name} onChange={(event) => { setName(event.target.value) }} />
-            <input placeholder="Description" name="Description" type="text" value={description} onChange={(event) => { setDescription(event.target.value) }} />
-            <button onClick={createPersona}>Create</button>
-            <div>{persona}</div>
+            <TextField placeholder="Name" required value={name} onChange={(event) => { setName(event.target.value) }} />
+            <TextField placeholder="Description" value={description} onChange={(event) => { setDescription(event.target.value) }} />
+            <Button onClick={createPersona}><AddCircleOutlineOutlinedIcon /> Create </Button>
+            <Box sx={{
+                bgcolor: indigo[900],
+                borderRadius: 3,
+                padding: 2,
+                width: '400px',
+                xheight: '200px',
+                color: 'white'
+            }}>
+                {creatingPersona ?
+                    <LinearProgress />
+                    :
+                    persona === "" ? <i>Create persona</i> : persona
+                }
+            </Box>
         </div>
     )
 }
-
-// import React, { useState } from 'react';
-// import { Box, Button, Form, FormField, TextInput, Grommet } from 'grommet';
-
-// interface PersonaFormProps {
-//     onSubmit?: (name: string, age: number, occupation: string) => void;
-// }
-
-// const PersonaForm: React.FC<PersonaFormProps> = ({ onSubmit }) => {
-//     const [name, setName] = useState<string>('');
-//     const [age, setAge] = useState<number>(0);
-//     const [occupation, setOccupation] = useState<string>('');
-
-//     const handleSubmit = () => {
-//         onSubmit(name, age, occupation);
-//         setName('');
-//         setAge(0);
-//         setOccupation('');
-//     };
-
-//     return (
-
-//         <Grommet>
-//             <Box align="center" justify="center" pad="large">
-//                 <Form onSubmit={handleSubmit}>
-//                     <FormField label="Name">
-//                         <TextInput value={name} onChange={(event) => setName(event.target.value)} />
-//                     </FormField>
-//                     <FormField label="Age">
-//                         <TextInput type="number" value={age} onChange={(event) => setAge(Number(event.target.value))} />
-//                     </FormField>
-//                     <FormField label="Occupation">
-//                         <TextInput value={occupation} onChange={(event) => setOccupation(event.target.value)} />
-//                     </FormField>
-//                     <Box direction="row" justify="center" margin={{ top: 'medium' }}>
-//                         <Button type="submit" label="Submit" primary />
-//                     </Box>
-//                 </Form>
-//             </Box>
-//         </Grommet>
-//     );
-// };
-
-// export default PersonaForm;
--- tammsvik/src/pages/bravo.tsx
+++ tammsvik/src/pages/bravo.tsx
@@ -3,6 +3,12 @@ import { Configuration, OpenAIApi } from "openai";
 import React, { useState } from "react";
 import Persona from "../components/persona";
 import Dalle from "../components/dalle";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 
 
 
