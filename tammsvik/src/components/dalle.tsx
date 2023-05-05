6a97862345405b0511c32df154ead5d0893caa77
Emelie Blomquist <emelie.blomquist@knowit.se>
2023-05-05 15:11:26 +0200

    push

--- tammsvik/src/components/dalle.tsx
+++ tammsvik/src/components/dalle.tsx
@@ -6,6 +6,19 @@ export interface DalleInput{
     persona:string;
 }
 
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+const response = async () => {await openai.createImage({
+    prompt: "a white siamese cat",
+    n: 1,
+    size: "1024x1024",
+});
+};
+  const image_url = response.data.data[0].url;
+
 export default function Dalle(persona: DalleInput): JSX.Element{
 
     return(
