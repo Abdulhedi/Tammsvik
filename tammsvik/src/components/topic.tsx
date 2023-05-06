0356e2105ed5eec9b7abe57db26aafa62ef276fe
KNOWIT\jersun <jerker.sundberg@knowit.se>
2023-05-06 09:14:50 +0200

    topic  styled

--- tammsvik/src/components/topic.tsx
+++ tammsvik/src/components/topic.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { Button, TextField, Box } from '@mui/material';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import LinearProgress from '@mui/material/LinearProgress';
+import { indigo } from '@mui/material/colors';
 
 
 export interface ITopic{
@@ -15,8 +19,15 @@ async function CreateTopic(): Promise<void>{
 
     return(
         <div>
-            <input placeholder="Topic" type="text" onChange={(event) => { setTopic(event.target.value) }} />
-            <button onClick={CreateTopic} >Submit</button>
+            <TextField placeholder="Topic" type="text" onChange={(event) => { setTopic(event.target.value) }} />
+            <Button onClick={CreateTopic}  sx={{
+                bgcolor: indigo[900],
+                borderRadius: 3,
+                padding: 2,
+                width: '200px',
+                xheight: '200px',
+                color: 'white'
+            }} >Submit</Button>
         </div>
     )
 }
